fc04b7ba28ba023c12b72d0dd074d8138a0972f6
krishna.wattamwar <krishna.wattamwar@adapty.com>
2021-10-04 11:39:45 +0530

    checking qulity check

--- src/test.ts
+++ src/test.ts
@@ -25,10 +25,3 @@ getTestBed().initTestEnvironment(
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
-
-
-// Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
-
